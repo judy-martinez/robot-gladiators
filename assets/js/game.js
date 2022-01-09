8f70e5e2d5eea37e4b3ff3314c8beda07b2b61d0
Judy <judy.mtz1@gmail.com>
2022-01-09 03:42:38 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,11 +72,11 @@ var fight = function(enemyName) {
 
 var startGame = function() {
     //reset player stats
-    playerHealth = 100;
-    playerAttack = 10;
-    playerMoney = 10;
-    
-    for(var i = 0; i < enemyNames.length; i++){
+    //playerHealth = 100;
+    //playerAttack = 10;
+    //playerMoney = 10;
+
+    for (var i = 0; i < enemyNames.length; i++) {
         // if player is still alive, keep fighting
         if (playerHealth > 0) {
             // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it        
@@ -95,15 +95,42 @@ var startGame = function() {
             fight(pickedEnemyName);
         }
         // if player isn't alive, stop the game
-        else {
-            window.alert('You have lost your robot in battle! Game Over!')
-            break;
+        //else {
+            //window.alert('You have lost your robot in battle! Game Over!')
+            //break;
+        //}
+
+        // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+        endGame();
+    };
+
+    // function to end the entire game
+    var endGame = function() {
+        // if player is still alive, player wins!
+        if (playerHealth > 0) {
+            window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ". ");
         }
+        else {
+            window.alert("You've lost your robot in battle.");
+        }
+    }
+
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();    
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 
     // play again
     // startGame();
 };
+
+
 
 // start the game when the page loads
 startGame();
